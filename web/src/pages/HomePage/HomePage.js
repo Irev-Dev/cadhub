72ef8347e936672c48f1d03e39b63672bf4bc2c7
Kurt Hutten <kurt@kurthutten.com>
2020-10-11 07:26:08 +1100

    get editor running somewhat

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,11 +1,15 @@
 import BlogLayout from 'src/layouts/BlogLayout'
 import BlogPostsCell from 'src/components/BlogPostsCell'
+import { initialize} from 'src/cascade/js/MainPage/CascadeMain'
 
 const HomePage = () => {
   return (
 
     <BlogLayout>
       <BlogPostsCell/>
+      <div>
+        <button onClick={() => new initialize()}>init</button>
+      </div>
     </BlogLayout>
   )
 }
