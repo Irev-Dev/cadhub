2df4ac0d57ddb0979d749bd475a02296bc0bd145
Kurt Hutten <kurt@kurthutten.com>
2020-10-16 16:43:29 +1100

    Add basic part page

---
+++ web/src/pages/IdePartPage/IdePartPage.js
@@ -0,0 +1,13 @@
+import { Link, routes } from '@redwoodjs/router'
+import MainLayout from 'src/layouts/MainLayout'
+import IdePartCell from 'src/components/IdePartCell'
+
+const IdePartPage = ({id}) => {
+  return (
+    <MainLayout>
+      <IdePartCell id={id} />
+    </MainLayout>
+  )
+}
+
+export default IdePartPage
