31ffc9058a272ec65747fa79cf10399c30696cea
Brandon Lyons <lyonsbp@umich.edu>
2020-10-28 10:37:45 -0400

    Correct color classes

--- web/src/components/UserPart/UserPart.js
+++ web/src/components/UserPart/UserPart.js
@@ -1,14 +1,14 @@
 function UserPart({ userName, partName }) {
   return (
-    <h3 className="text-xl">
-    <span className=" border-2 border-purple-900 mr-1"></span>
+    <h3 className="text-xl font-roboto">
+    <span className=" border-2 border-indigo-800 mr-1"></span>
       <span className="text-gray-500">
         {userName}
       </span>
-      <span className="text-2xl mx-1 text-gray-300">
+      <span className="text-2xl mx-1 text-gray-400">
         /
       </span>
-      <span className="text-purple-900">
+      <span className="text-indigo-800">
         {partName}
       </span>
     </h3>
