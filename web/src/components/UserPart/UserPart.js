30bac51c865421cdb127eadfa001b987e764ce3a
Brandon Lyons <lyonsbp@umich.edu>
2020-10-28 11:14:07 -0400

    Add transform and skew classes

--- web/src/components/UserPart/UserPart.js
+++ web/src/components/UserPart/UserPart.js
@@ -1,12 +1,11 @@
 function UserPart({ userName, partName }) {
   return (
     <h3 className="text-xl font-roboto">
-    <span className=" border-2 border-indigo-800 mr-1"></span>
+    <span className="border-2 border-indigo-800 mr-1"></span>
       <span className="text-gray-500">
         {userName}
       </span>
-      <span className="text-2xl mx-1 text-gray-400">
-        /
+      <span className="border-2 border-gray-400 mx-1 transform -skew-x-12">
       </span>
       <span className="text-indigo-800">
         {partName}
