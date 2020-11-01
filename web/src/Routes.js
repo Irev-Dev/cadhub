55f9dfd6dea2ad6e0f9ea9a47d07b39cee7e594f
Kurt Hutten <kurt@kurthutten.com>
2020-11-01 18:05:19 +1100

    Wipe slate clean

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -12,30 +12,8 @@ import { Router, Route, Private } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
-      {/* TODO add add min role to users and users/new  */}
-      <Private unauthenticated="home" role="admin">
-        <Route path="/users" page={UsersPage} name="users" />
-        <Route path="/users/new" page={NewUserPage} name="newUser" />
-      </Private>
-      <Private unauthenticated="home">
-        <Route path="/users/{id:Int}/edit" page={EditUserPage} name="editUser" />
-      </Private>
-      <Route path="/users/{id:Int}" page={UserPage} name="user" />
-      <Route path="/contact" page={ContactPage} name="contact" />
-      <Route path="/parts/new" page={NewPartPage} name="newPart" />
-      <Route path="/parts/{id:Int}/edit" page={EditPartPage} name="editPart" />
-      <Route path="/parts/{id:Int}/ide" page={IdePartPage} name="partIde" />
-      <Route path="/parts/{id:Int}" page={PartPage} name="part" />
-      <Route path="/parts" page={PartsPage} name="parts" />
-      <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
-      <Private unauthenticated="home">
-        <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
-        <Route path="/admin/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
-        <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
-        <Route path="/admin/posts" page={PostsPage} name="posts" />
-      </Private>
-      <Route path="/about" page={AboutPage} name="about" />
       <Route path="/" page={PartsPage} name="home" />
+      <Route path="/blah/*" page={PartsPage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
