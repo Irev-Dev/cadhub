198c40132fee6793130fabd071bb52d89580711f
Kurt Hutten <kurt@kurthutten.com>
2020-11-10 20:37:04 +1100

    Fix new button,
    also redirects if user is not signed in

--- web/src/pages/NewPart2Page/NewPart2Page.js
+++ web/src/pages/NewPart2Page/NewPart2Page.js
@@ -1,11 +1,16 @@
+import { useEffect } from 'react'
 import { useAuth } from '@redwoodjs/auth'
+import { navigate, routes } from '@redwoodjs/router'
 
 import MainLayout from 'src/layouts/MainLayout'
 import Part2Cell from 'src/components/Part2Cell'
 
 
 const NewPart2Page = ({userName}) => {
-  const { currentUser } = useAuth()
+  const { isAuthenticated, currentUser } = useAuth()
+  useEffect(() => {!isAuthenticated &&
+    navigate(routes.home())},
+  [currentUser])
   return (
     <MainLayout>
       <Part2Cell
