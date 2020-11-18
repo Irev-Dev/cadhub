374984a01c03b262bfb84a876f994d22ff38bfcf
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-11-18 14:12:49 -0300

    :mag: chore: some standard meta tags moved to index.html

--- web/src/components/Seo/Seo.js
+++ web/src/components/Seo/Seo.js
@@ -14,13 +14,9 @@ const Seo = ({ title, description, lang }) => {
         <meta property="og:locale" content={lang} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:type" content="website" />
-        <meta name="robots" content="index, follow" />
         <meta name="description" content={description} />
-        <meta name="keywords" content="cadhub, app, application, web application, cad, code, code-cad, cad-modeling, community, website, cascade studio, openscad" />
         <title>Cadhub - {title}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-     </Helmet>
+      </Helmet>
     </>
   )
 }
