f0cba9c1e5cbcef756d666ff29c843702550afeb
Kurt Hutten <kurt@kurthutten.com>
2020-12-19 18:25:44 +1100

    issue-167 Add admin parts page
    resolves #167

---
+++ web/src/components/AdminPartsCell/AdminPartsCell.js
@@ -0,0 +1,39 @@
+import { Link, routes } from '@redwoodjs/router'
+
+import AdminParts from 'src/components/AdminParts'
+
+export const QUERY = gql`
+  query PARTS {
+    parts {
+      id
+      title
+      description
+      code
+      mainImage
+      createdAt
+      updatedAt
+      userId
+      deleted
+      user {
+        userName
+      }
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => {
+  return (
+    <div className="rw-text-center">
+      {'No parts yet. '}
+      <Link to={routes.newPart()} className="rw-link">
+        {'Create one?'}
+      </Link>
+    </div>
+  )
+}
+
+export const Success = ({ parts }) => {
+  return <AdminParts parts={parts} />
+}
---
+++ web/src/pages/AdminPartsPage/AdminPartsPage.js
@@ -0,0 +1,12 @@
+import MainLayout from 'src/layouts/MainLayout'
+import AdminPartsCell from 'src/components/AdminPartsCell'
+
+const PartsPage = () => {
+  return (
+    <MainLayout>
+      <AdminPartsCell />
+    </MainLayout>
+  )
+}
+
+export default PartsPage
