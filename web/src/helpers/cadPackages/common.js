7b104a812df90c619103291025c32f2c4cc86410
Kurt Hutten <kurt@kurthutten.com>
2021-04-27 19:52:49 +1000

    Update lamda base url

--- web/src/helpers/cadPackages/common.js
+++ web/src/helpers/cadPackages/common.js
@@ -1,3 +1,3 @@
 export const lambdaBaseURL =
   process.env.CAD_LAMBDA_BASE_URL ||
-  'https://t7wdlz8ztf.execute-api.us-east-1.amazonaws.com/dev'
+  'https://wzab9s632b.execute-api.us-east-1.amazonaws.com/prod'
