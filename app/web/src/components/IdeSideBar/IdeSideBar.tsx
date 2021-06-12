6b4ff7aa61045017dbefe37552d77c660e65030e
Kurt Hutten <k.hutten@protonmail.ch>
2021-06-12 19:24:42 +1000

    Make new favicon into a homepage link
    related to #360

--- app/web/src/components/IdeSideBar/IdeSideBar.tsx
+++ app/web/src/components/IdeSideBar/IdeSideBar.tsx
@@ -1,10 +1,13 @@
+import { Link, routes } from '@redwoodjs/router'
 import Svg from 'src/components/Svg/Svg'
 
 const IdeSideBar = () => {
   return (
     <div className="h-full flex flex-col justify-between">
       <div className="w-16 h-16 flex items-center justify-center bg-gray-900">
-          <Svg className="w-12" name="favicon" />
+          <Link to={routes.home()}>
+            <Svg className="w-12" name="favicon" />
+          </Link>
         </div>
       <button className=" text-gray-300 p-2 pb-4 flex justify-center" aria-label="IDE settings">
         <Svg name="big-gear" />
