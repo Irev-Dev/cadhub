709c653afb5ea516ea8e10eeffd25f27a1bb8387
Kurt Hutten <k.hutten@protonmail.ch>
2021-08-14 15:31:04 +1000

    add card to ide too

--- app/web/src/pages/IdeProjectPage/IdeProjectPage.tsx
+++ app/web/src/pages/IdeProjectPage/IdeProjectPage.tsx
@@ -1,10 +1,15 @@
 import IdeProjectCell from 'src/components/IdeProjectCell'
 import Seo from 'src/components/Seo/Seo'
+import { makeSocialPublicId } from 'src/helpers/hooks/useUpdateSocialCard'
 
 const IdeProjectPage = ({ userName, projectTitle }) => {
+  const socialImageUrl = `http://res.cloudinary.com/irevdev/image/upload/c_scale,w_1200/v1/CadHub/${makeSocialPublicId(
+    userName,
+    projectTitle
+  )}`
   return (
     <>
-      <Seo title={projectTitle} description={projectTitle} lang="en-US" />
+      <Seo title={projectTitle} description={projectTitle} socialImageUrl={socialImageUrl} lang="en-US" />
       <IdeProjectCell userName={userName} projectTitle={projectTitle} />
     </>
   )
