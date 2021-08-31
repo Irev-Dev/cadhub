db9270b7ceb3801a49aad12acf57dc76f40f8bff
Kurt Hutten <k.hutten@protonmail.ch>
2021-08-31 17:19:14 +1000

    Fix doc build

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -2,4 +2,5 @@ const webConfig = require('../app/web/tailwind.config.js')
 module.exports = {
   ...webConfig,
   purge: ['./src/**/*.html', './src/**/*.js', './src/**/*.ts', './src/**/*.tsx', './blog/**/*.md', './blog/**/*.mdx', './docs/**/*.md', './docs/**/*.mdx', './src/**/*.css'],
+  plugins: []
 }
