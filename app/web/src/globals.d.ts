59df7fdc254bf27c149ced6a34e015bacc859f2b
Frank Johnson <frankjohnson1993@gmail.com>
2021-09-19 01:07:46 -0400

    Added globals.d.ts

---
+++ app/web/src/globals.d.ts
@@ -0,0 +1,7 @@
+// While the raw-loader Webpack plugin actually makes these imports work, this
+// eliminates noisy TypeScript errors by registering these file endings as types.
+// Learned this method of registering modules from https://stackoverflow.com/a/57444766
+declare module "*.md";
+declare module "*.SCAD";
+declare module "*.py";
+declare module "*.jscad";
